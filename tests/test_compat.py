5b17fc6e16ed29764cc5c98a33933b0806053641
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-08-25 15:40:25 +0200

    Remove import magic

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -23,8 +23,45 @@ def test_old_imports():
 
 def test_exposed_werkzeug_mod():
     import werkzeug
-    for key in werkzeug.__all__:
-        # deprecated, skip it
-        if key in ('templates', 'Template'):
-            continue
+    assert _expected_all == set(werkzeug.__all__)
+    for key in _expected_all:
         getattr(werkzeug, key)
+
+
+_expected_all = frozenset((
+    'OrderedMultiDict', 'ImmutableOrderedMultiDict', 'ETagResponseMixin',
+    'http_date', 'create_environ', 'redirect', 'ETagRequestMixin',
+    'quote_etag', 'BaseResponse', 'FileStorage', 'UserAgentMixin',
+    'parse_options_header', 'make_line_iter', 'url_unquote_plus',
+    'HTTP_STATUS_CODES', 'get_host', 'FileWrapper', 'Headers', 'FileMultiDict',
+    'uri_to_iri', 'url_encode', 'ImmutableTypeConversionDict',
+    'is_hop_by_hop_header', 'ResponseStreamMixin',
+    'parse_authorization_header', 'parse_date', 'Aborter', 'CharsetAccept',
+    'ResponseCacheControl', 'url_quote', 'ImmutableList', 'is_entity_header',
+    'LocalStack', 'EnvironHeaders', 'run_wsgi_app', 'WWWAuthenticate',
+    'SharedDataMiddleware', 'check_password_hash', 'EnvironBuilder',
+    'BaseRequest', 'parse_form_data', 'wrap_file', 'import_string',
+    'HeaderSet', 'ETags', 'dump_options_header', 'url_decode',
+    'header_property', 'pop_path_info', 'MultiDict', 'find_modules',
+    'CommonRequestDescriptorsMixin', 'RequestCacheControl', 'ImmutableDict',
+    'generate_etag', 'Local', 'Authorization', 'unquote_header_value',
+    'unquote_etag', 'generate_password_hash', 'AuthorizationMixin',
+    'bind_arguments', 'environ_property', 'ImmutableMultiDict',
+    'append_slash_redirect', 'MIMEAccept', 'validate_arguments',
+    'get_current_url', 'parse_etags', 'CombinedMultiDict', 'LimitedStream',
+    'parse_www_authenticate_header', 'cached_property', 'run_simple',
+    'parse_cache_control_header', 'ClosingIterator', 'Client', 'unescape',
+    'iri_to_uri', 'remove_entity_headers', 'url_quote_plus', 'dump_cookie',
+    'Response', 'DebuggedApplication', 'release_local', 'Href',
+    'quote_header_value', 'remove_hop_by_hop_headers', 'test_app', 'escape',
+    'LocalProxy', 'abort', 'dump_header', 'secure_filename',
+    'CommonResponseDescriptorsMixin', 'peek_path_info', 'parse_dict_header',
+    'format_string', 'HTMLBuilder', 'cookie_date', 'html',
+    'WWWAuthenticateMixin', 'UserAgent', 'xhtml', 'extract_path_info',
+    'parse_accept_header', 'Request', 'LanguageAccept', 'LocalManager',
+    'DispatcherMiddleware', 'ArgumentValidationError', 'url_unquote',
+    'AcceptMixin', '_easteregg', 'url_fix', 'responder', 'Accept',
+    'parse_list_header', 'TypeConversionDict', 'is_resource_modified',
+    'CallbackDict', 'parse_set_header', 'parse_cookie', 'exceptions',
+    'routing', 'script'
+))
--- werkzeug/__init__.py
+++ werkzeug/__init__.py
@@ -14,141 +14,78 @@
     :copyright: (c) 2014 by the Werkzeug Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
-from types import ModuleType
-import sys
-
-from werkzeug._compat import iteritems
 
 # the version.  Usually set automatically by a script.
 __version__ = '0.10-dev'
 
+from . import exceptions, routing, script
 
-# This import magic raises concerns quite often which is why the implementation
-# and motivation is explained here in detail now.
-#
-# The majority of the functions and classes provided by Werkzeug work on the
-# HTTP and WSGI layer.  There is no useful grouping for those which is why
-# they are all importable from "werkzeug" instead of the modules where they are
-# implemented.  The downside of that is, that now everything would be loaded at
-# once, even if unused.
-#
-# The implementation of a lazy-loading module in this file replaces the
-# werkzeug package when imported from within.  Attribute access to the werkzeug
-# module will then lazily import from the modules that implement the objects.
+from .serving import run_simple
 
+from .testapp import test_app
 
-# import mapping to objects in other modules
-all_by_module = {
-    'werkzeug.debug':       ['DebuggedApplication'],
-    'werkzeug.local':       ['Local', 'LocalManager', 'LocalProxy',
-                             'LocalStack', 'release_local'],
-    'werkzeug.serving':     ['run_simple'],
-    'werkzeug.test':        ['Client', 'EnvironBuilder', 'create_environ',
-                             'run_wsgi_app'],
-    'werkzeug.testapp':     ['test_app'],
-    'werkzeug.exceptions':  ['abort', 'Aborter'],
-    'werkzeug.urls':        ['url_decode', 'url_encode', 'url_quote',
-                             'url_quote_plus', 'url_unquote',
-                             'url_unquote_plus', 'url_fix', 'Href',
-                             'iri_to_uri', 'uri_to_iri'],
-    'werkzeug.formparser':  ['parse_form_data'],
-    'werkzeug.utils':       ['escape', 'environ_property',
-                             'append_slash_redirect', 'redirect',
-                             'cached_property', 'import_string',
-                             'dump_cookie', 'parse_cookie', 'unescape',
-                             'format_string', 'find_modules', 'header_property',
-                             'html', 'xhtml', 'HTMLBuilder',
-                             'validate_arguments', 'ArgumentValidationError',
-                             'bind_arguments', 'secure_filename'],
-    'werkzeug.wsgi':        ['get_current_url', 'get_host', 'pop_path_info',
-                             'peek_path_info', 'SharedDataMiddleware',
-                             'DispatcherMiddleware', 'ClosingIterator',
-                             'FileWrapper', 'make_line_iter', 'LimitedStream',
-                             'responder', 'wrap_file', 'extract_path_info'],
-    'werkzeug.datastructures': ['MultiDict', 'CombinedMultiDict', 'Headers',
-                             'EnvironHeaders', 'ImmutableList',
-                             'ImmutableDict', 'ImmutableMultiDict',
-                             'TypeConversionDict', 'ImmutableTypeConversionDict',
-                             'Accept', 'MIMEAccept', 'CharsetAccept',
-                             'LanguageAccept', 'RequestCacheControl',
-                             'ResponseCacheControl', 'ETags', 'HeaderSet',
-                             'WWWAuthenticate', 'Authorization',
-                             'FileMultiDict', 'CallbackDict', 'FileStorage',
-                             'OrderedMultiDict', 'ImmutableOrderedMultiDict'],
-    'werkzeug.useragents':  ['UserAgent'],
-    'werkzeug.http':        ['parse_etags', 'parse_date', 'http_date',
-                             'cookie_date', 'parse_cache_control_header',
-                             'is_resource_modified', 'parse_accept_header',
-                             'parse_set_header', 'quote_etag', 'unquote_etag',
-                             'generate_etag', 'dump_header',
-                             'parse_list_header', 'parse_dict_header',
-                             'parse_authorization_header',
-                             'parse_www_authenticate_header',
-                             'remove_entity_headers', 'is_entity_header',
-                             'remove_hop_by_hop_headers', 'parse_options_header',
-                             'dump_options_header', 'is_hop_by_hop_header',
-                             'unquote_header_value',
-                             'quote_header_value', 'HTTP_STATUS_CODES'],
-    'werkzeug.wrappers':    ['BaseResponse', 'BaseRequest', 'Request',
-                             'Response', 'AcceptMixin', 'ETagRequestMixin',
-                             'ETagResponseMixin', 'ResponseStreamMixin',
-                             'CommonResponseDescriptorsMixin',
-                             'UserAgentMixin', 'AuthorizationMixin',
-                             'WWWAuthenticateMixin',
-                             'CommonRequestDescriptorsMixin'],
-    'werkzeug.security':    ['generate_password_hash', 'check_password_hash'],
-    # the undocumented easteregg ;-)
-    'werkzeug._internal':   ['_easteregg']
-}
+from .useragents import UserAgent
 
-# modules that should be imported when accessed as attributes of werkzeug
-attribute_modules = frozenset(['exceptions', 'routing', 'script'])
+from ._internal import _easteregg
 
+from .debug import DebuggedApplication
 
-object_origins = {}
-for module, items in iteritems(all_by_module):
-    for item in items:
-        object_origins[item] = module
+from .datastructures import Accept, Authorization, CallbackDict, CharsetAccept, \
+    CombinedMultiDict, ETags, EnvironHeaders, FileMultiDict, FileStorage, \
+    HeaderSet, Headers, ImmutableDict, ImmutableList, ImmutableMultiDict, \
+    ImmutableOrderedMultiDict, ImmutableTypeConversionDict, LanguageAccept, \
+    MIMEAccept, MultiDict, OrderedMultiDict, RequestCacheControl, \
+    ResponseCacheControl, TypeConversionDict, WWWAuthenticate
 
+from .utils import ArgumentValidationError, HTMLBuilder, append_slash_redirect, \
+    bind_arguments, cached_property, dump_cookie, environ_property, escape, \
+    find_modules, format_string, header_property, html, import_string, \
+    parse_cookie, redirect, secure_filename, unescape, validate_arguments, \
+    xhtml
 
-class module(ModuleType):
-    """Automatically import objects from the modules."""
+from .wrappers import AcceptMixin, AuthorizationMixin, BaseRequest, \
+    BaseResponse, CommonRequestDescriptorsMixin, \
+    CommonResponseDescriptorsMixin, ETagRequestMixin, ETagResponseMixin, \
+    Request, Response, ResponseStreamMixin, UserAgentMixin, \
+    WWWAuthenticateMixin
 
-    def __getattr__(self, name):
-        if name in object_origins:
-            module = __import__(object_origins[name], None, None, [name])
-            for extra_name in all_by_module[module.__name__]:
-                setattr(self, extra_name, getattr(module, extra_name))
-            return getattr(module, name)
-        elif name in attribute_modules:
-            __import__('werkzeug.' + name)
-        return ModuleType.__getattribute__(self, name)
+from .local import Local, LocalManager, LocalProxy, LocalStack, release_local
 
-    def __dir__(self):
-        """Just show what we want to show."""
-        result = list(new_module.__all__)
-        result.extend(('__file__', '__path__', '__doc__', '__all__',
-                       '__docformat__', '__name__', '__path__',
-                       '__package__', '__version__'))
-        return result
+from .security import check_password_hash, generate_password_hash
 
-# keep a reference to this module so that it's not garbage collected
-old_module = sys.modules['werkzeug']
+from .test import Client, EnvironBuilder, create_environ, run_wsgi_app
 
+from .wsgi import ClosingIterator, DispatcherMiddleware, FileWrapper, \
+    LimitedStream, SharedDataMiddleware, extract_path_info, get_current_url, \
+    get_host, make_line_iter, peek_path_info, pop_path_info, responder, \
+    wrap_file
 
-# setup the new module and patch it into the dict of loaded modules
-new_module = sys.modules['werkzeug'] = module('werkzeug')
-new_module.__dict__.update({
-    '__file__':         __file__,
-    '__package__':      'werkzeug',
-    '__path__':         __path__,
-    '__doc__':          __doc__,
-    '__version__':      __version__,
-    '__all__':          tuple(object_origins) + tuple(attribute_modules),
-    '__docformat__':    'restructuredtext en'
-})
+from .http import HTTP_STATUS_CODES, cookie_date, dump_header, \
+    dump_options_header, generate_etag, http_date, is_entity_header, \
+    is_hop_by_hop_header, is_resource_modified, parse_accept_header, \
+    parse_authorization_header, parse_cache_control_header, parse_date, \
+    parse_dict_header, parse_etags, parse_list_header, parse_options_header, \
+    parse_set_header, parse_www_authenticate_header, quote_etag, \
+    quote_header_value, remove_entity_headers, remove_hop_by_hop_headers, \
+    unquote_etag, unquote_header_value
 
+from .urls import Href, iri_to_uri, uri_to_iri, url_decode, url_encode, \
+    url_fix, url_quote, url_quote_plus, url_unquote, url_unquote_plus
 
-# Due to bootstrapping issues we need to import exceptions here.
-# Don't ask :-(
-__import__('werkzeug.exceptions')
+from .formparser import parse_form_data
+
+from .exceptions import Aborter, abort
+
+def _get_all():
+    for name, obj in globals().items():
+        if name.startswith('_') and name != '_easteregg':
+            continue
+        if not hasattr(obj, '__module__') and \
+           name not in ('exceptions', 'routing', 'script',
+                        'HTTP_STATUS_CODES'):
+            # Exclude most modules. HTTP_STATUS_CODES is also in here because
+            # it is mistakenly identified as a module.
+            continue
+        yield name
+
+__all__ = tuple(_get_all())
